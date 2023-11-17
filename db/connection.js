2d5953b3982a50999a627922ddaf0399e54f34e1
Pranesh [ PIPIN ] <117482836+pipinthedev@users.noreply.github.com>
2023-11-17 07:26:56 +0530

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -2,10 +2,10 @@ var mysql = require("mysql");
 var config;
 config = {
   mysql_pool: mysql.createPool({
-    host: 'NULL',
-    user: 'athulslv_muthukumar',
-    password: 'Athulya@123',
-    database: 'athulslv_sal_subscriber102',
+    host: '162.241.123.158',
+    user: 'theatgg6_pranesh',
+    password: '021230Srp!',
+    database: 'theatgg6_sal_subscriber102',
     multipleStatements: true,
     connectionLimit: 100, 
   }),
